8b789c32325c3a764163b01f7a74e00e64b7a724
Marco Paganini <paganini@paganini.net>
2024-06-30 12:51:48 -0700

    Fix the path in publish.sh

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -eu
-hugo && cd public && rsync -avp --delete --exclude .git . "${HOME}/github/marcopaganini/marcopaganini.github.io"
+hugo && cd public && rsync -avp --delete --exclude .git . "${HOME}/src/github.com/marcopaganini/marcopaganini.github.io"
